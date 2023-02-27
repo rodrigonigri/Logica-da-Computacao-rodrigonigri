822ab5dfdf2cea2fa947e3ce64f4d21e027a01be
Rodrigo Nigri Griner <rodrigong1@al.insper.edu.br>
2023-02-27 00:51:23 -0300

    segunda tentativ

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import sys
 
-# get arguments from command line
-args = sys.argv
-
-
 def print_colored(text, color): # so pra debugar
     """Print text in color"""
     if color == "red":
@@ -26,76 +22,89 @@ def print_colored(text, color): # so pra debugar
         print("\033[99m {}\033[00m" .format(text))
 
 class Token():
-    def _init_(self, type, value): # type is a string, value is a int
+    def __init__(self, type, value): # type is a string, value is a int
         self.type = type
         self.value = value 
 
 
 class Tokenizer():
-    def _init_(self, source):
+    def __init__(self, source):
         self.source = source
         self.position = 0
         self.next = None
 
+
     def selectNext(self):
-        operacoes = "+-"
-        numeros = "0123456789"
+        flag_token = True
 
-
-        if self.position >= len(self.source):
-            return Token("EOF", None)
-        
-        else:
-
+        try:
             char = self.source[self.position]
+        except:
+            self.next = Token("EOF", "")
+            return
 
-            self.position += 1
-            if char in operacoes:
-                if char == "+":
-                    self.next = Token("PLUS", "+")
-                elif char == "-":
-                    self.next = Token("MINUS", "-")
-            
-            elif char in numeros:
-            
-                number = ""
-                while char in numeros:
-                    number += char
-                    char = self.source[self.position]
-                    self.position += 1
-
-                self.next = Token("INT", int(number))
-                self.position -= 1
-
-            elif char == " ":
+        if char in "0123456789":
+            flag_token = True
+            numero = ""
+            numero += char
+            while flag_token: # vai percorrendo algarismo por algarismo até encontrar um espaço ou um operador
                 self.position += 1
-                self.selectNext()
+                try:
+                    char = self.source[self.position]
+                except:
+                    self.next = Token("INT", int(numero))
+                    return
+
+                if char == " " or char in "+-":
+                    flag_token = False
+                    self.next = Token("INT", int(numero))
+                else:
+                    numero += char
+
+        elif char in "+-":
+            if char == "-":
+                self.next = Token("MINUS", "-")
+            
+            elif char == "+":
+                self.next = Token("PLUS", "+")
+            
+            self.position += 1
+            
+        elif char == " ": # se for um espaço
+            self.position += 1
+            self.selectNext()
+        
+
 
 class Parser():
-    tokenizer = None
+
     @staticmethod
     def parseExpression():
         token_agora = Parser.tokenizer.next
+        res = token_agora.value
+
         if token_agora.type == "INT":
-            res = token_agora.value
             Parser.tokenizer.selectNext()
             token_agora = Parser.tokenizer.next
 
-            while token_agora.type == "PLUS" or token_agora.type == "MINUS":
+
+            while token_agora.type == "MINUS" or token_agora.type == "PLUS":
                 if token_agora.type == "MINUS":
                     Parser.tokenizer.selectNext()
                     token_agora = Parser.tokenizer.next
                     if token_agora.type == "INT":
-                        res -= token_agora.value
-                    else:
+                        res -= Parser.tokenizer.next.value
+
+                    elif token_agora.type != "INT":
                         raise Exception("Erro de sintaxe")
 
                 elif token_agora.type == "PLUS":
                     Parser.tokenizer.selectNext()
                     token_agora = Parser.tokenizer.next
                     if token_agora.type == "INT":
-                        res += token_agora.value
-                    else:
+                        res += Parser.tokenizer.next.value
+
+                    elif token_agora.type != "INT":
                         raise Exception("Erro de sintaxe")
                 
                 Parser.tokenizer.selectNext()
@@ -105,6 +114,8 @@ class Parser():
         
         else:
             raise Exception("Erro de sintaxe")
+        
+
     @staticmethod
     def run(codigo):
         Parser.tokenizer = Tokenizer(codigo)
@@ -113,9 +124,15 @@ class Parser():
 
         if Parser.tokenizer.next.type == "EOF":
             return resultado
+        
         else:
             raise Exception("Erro de sintaxe")
         
 
 parser = Parser()
-print(parser.run(sys.argv[1]))
+
+# get arguments from command line
+args = sys.argv
+
+print(parser.run(args[1]))
+
